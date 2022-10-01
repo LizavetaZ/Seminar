8a228d72725cef3a071f42d8d5dedb63ab97cab7
Лиза за <lizuska@yandex.ru>
2022-10-01 16:17:11 +0300

    find max among 3

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите числа");
+string value1 = Console.ReadLine();
+string value2 = Console.ReadLine();
+string value3 = Console.ReadLine();
+int a = int.Parse(value1);
+int b = int.Parse(value2);
+int c = int.Parse(value3);
+int max = a;
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+Console.Write("max=");
+Console.WriteLine(max);
+
