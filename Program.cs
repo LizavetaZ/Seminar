738c3262e8832dd14b22461e4ffd130019dc025b
Лиза за <lizuska@yandex.ru>
2022-10-01 16:14:16 +0300

    Find max

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите числа");
+string value1 = Console.ReadLine();
+string value2 = Console.ReadLine();
+int a = int.Parse(value1);
+int b = int.Parse(value2);
+int max = a;
+if (a>max) max = a;
+if (b>max) max = b;
+Console.Write("max=");
+Console.WriteLine(max);
+
